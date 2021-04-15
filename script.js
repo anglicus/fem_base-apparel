0bb4abcee354eb5f4e76ca90412247bb9961bfc5
James English <64441712+anglicus@users.noreply.github.com>
2021-04-15 09:18:50 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,13 @@
 const form = document.getElementById('email-form');
 const input = document.getElementById('email');
-const button = document.getElementById('submit');
 const errormsg = document.getElementById('error-message');
 const erroricon = document.getElementById('error-icon');
-
-button.addEventListener('click', e => {
-
-    console.log('clicked');
-})
 
 input.addEventListener('invalid', e => {
     e.preventDefault();
     errormsg.classList.remove('hidden');
     erroricon.classList.remove('hidden');
     input.classList.add('invalid-input');
-    errormsg.innerHTML = "Please provide a valid email"
 })
 
 form.addEventListener('submit', e => {
